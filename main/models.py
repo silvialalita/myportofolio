1876ea7b05bde3ec90bb7e23ebac616d7d448999
silvialalita_ <silvia.lalita@ui.ac.id>
2026-09-13 16:17:01 +0700

    feat: add education models

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 
 # Create your models here.
+# Model bertugas untuk mengatur dan mengelola data pada sebuah aplikasi.
 class Experience(models.Model):
     EXPERIENCE_CHOICES = [
         ('internship', 'Internship'),
@@ -21,6 +22,28 @@ class Experience(models.Model):
     ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.title
+    
+    @property
+    def is_ongoing(self):
+        return self.ended_at is None
+
+class Education(models.Model):
+    EDUCATION_TYPES = [
+        ('formal', 'Formal Education'),
+        ('program', 'Educational Program'),
+        ('certification', 'Certification'),
+        ('course', 'Course / Training'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    institution = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=30, choices=EDUCATION_TYPES, default='formal')
+    thumbnail = models.URLField(blank=True, default='')
+    started_at = models.DateTimeField(auto_now_add=True)
+    ended_at = models.DateTimeField(blank=True, null=True)
+    def __str__(self):
+        return self.institution
     
     @property
     def is_ongoing(self):
--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-from main.models import Experience
+from main.models import Experience, Education
 
 # Create your views here.
+# View menangani logika yang akan ditampilkan kepada pengguna. 
 def show_main(request):
     context = {
         "name": "Silvia Lalita Damayanti",
+        "first_name": "Silvia",
+        "nick_name": "Lalita",
+        "last_name": "Damayanti",
         "npm": "2506621863",
         "study_program": "S1 Ilmu Komputer",
         "bio": (
@@ -22,3 +27,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    context = {
+        "name": "Silvia Lalita Damayanti",
+        "education_list": Education.objects.all(),
+    }
+    return render(request, "education.html", context)
