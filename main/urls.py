1b7f16cfc22cf10fdc0656c98bc007af03315965
silvialalita_ <silvia.lalita@ui.ac.id>
2026-09-21 00:20:37 +0700

    feat: add experience management features

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education, show_projects, create_project, get_projects_json, delete_project
-
+from main.views import(
+    show_main, 
+    show_experience, create_experience, get_experiences_json, delete_experience,
+    show_education, 
+    show_projects, create_project, get_projects_json, delete_project,
+)
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
+
     path("experience/", show_experience, name="show_experience"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path("api/experience/", get_experiences_json, name="get_experiences_json"),
+    path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
+
     path("education/", show_education, name="show_education"),
+
     path("projects/", show_projects, name="show_projects"),
     path("projects/add/", create_project, name="create_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
-from main.forms import ProjectForm
+from main.forms import ProjectForm, ExperienceForm
 from main.models import Experience, Education, Project
 
 # Create your views here.
@@ -24,13 +24,64 @@ def show_main(request):
     return render(request, "index.html", context)
 
 
+
+# EXPERIENCE
 def show_experience(request):
+    json_response = get_experiences_json(request)
+
+    experiences = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    experiences = [experience.object for experience in experiences]
+    title_query = request.GET.get("title", "").strip()
+    
     context = {
         "name": "Silvia Lalita Damayanti",
-        "experience_list": Experience.objects.all(),
+        "experience_list": experiences,
+        "title_query": title_query,
     }
     return render(request, "experience.html", context)
 
+def create_experience(request):
+    form = ExperienceForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "New experience added successfully!")
+        return redirect("main:show_experience")
+
+    context = {
+        "name": "Silvia Lalita Damayanti",
+        "form": form,
+    }
+    return render(request, "experience_form.html", context)
+
+def get_experiences_json(request):
+    title_query = request.GET.get("title", "").strip()
+    experiences = Experience.objects.all()
+
+    if title_query:
+        experiences = experiences.filter(title__icontains=title_query)
+
+    experiences_json = serializers.serialize("json", experiences)
+    return HttpResponse(experiences_json, content_type="application/json")
+
+
+
+def delete_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    if request.method == "POST":
+        experience.delete()
+        messages.success(request, "Experience deleted successfully!")
+        return redirect("main:show_experience")
+
+    return redirect("main:show_experience")
+
+
+
+# PROJECT
 def create_project(request):
     form = ProjectForm(request.POST or None)
 
@@ -57,7 +108,7 @@ def show_projects(request):
 
     context = {
         "name": "Silvia Lalita Damayanti",
-        "project_list": Project.objects.all(),
+        "project_list": projects,
         "title_query": title_query,
     }
     return render(request, "project.html", context)
@@ -82,6 +133,9 @@ def delete_project(request, project_id):
 
     return redirect("main:show_projects")
 
+
+
+# EDUCATION
 def show_education(request):
     context = {
         "name": "Silvia Lalita Damayanti",
